c45d1f403dc6561f5be3e52033e7543babbc8ec4
jerumanu <emmanueljeru200@gmail.com>
2020-11-06 18:46:27 +0300

    add componenets

--- src/app/qoute.ts
+++ src/app/qoute.ts
@@ -1,2 +1,7 @@
 export class Qoute {
+    showDescription:boolean
+    constructor( public name:string ,public description:string, public completeDate:Date)
+    {
+        this.showDescription=false
+    }
 }
--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -1,12 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Qoute } from "../qoute";
 @Component({
   selector: 'app-qoute',
   templateUrl: './qoute.component.html',
   styleUrls: ['./qoute.component.css']
 })
 export class QouteComponent implements OnInit {
-
+qoutes :Qoute[] =[
+  new Qoute ("victa","petience pays but bail",new Date(1994,4,2)),
+  new Qoute ("victa","petience pays but bail",new Date(1994,4,2)),
+  new Qoute ("victa","petience pays but bail",new Date(1994,4,2)),
+  new Qoute ("victa","petience pays but bail",new Date(1994,4,2)),
+  new Qoute ("victa","petience pays but bail",new Date(1994,4,2)),
+]
   constructor() { }
 
   ngOnInit(): void {
