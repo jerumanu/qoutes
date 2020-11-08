64274957f88d9991de41df010748bf31cb58df92
jerumanu <emmanueljeru200@gmail.com>
2020-11-08 22:27:27 +0300

    add changes

--- src/app/qoute/qoute.component.ts
+++ src/app/qoute/qoute.component.ts
@@ -7,20 +7,18 @@ import { Qoute } from "../qoute";
 })
 export class QouteComponent implements OnInit {
 qoutes :Qoute[] =[
-  new Qoute ("insperational","victor","petience pays but pains", new Date(2020,4,6)),
-  new Qoute ("love","tripplevic","there is no way to success but success is the way", new Date(2020 ,9, 10)),
-  new Qoute ("mortivational","manuu","do not boast about tommorrow for you do not know what a day may bring forth" ,new Date (2000,6,12)),
-  new Qoute ("love","victa","petience pays but bail" ,new Date(2001,4,7)),
-  new Qoute ("insperational","victa","petience pays but bail" ,new Date (1990,6,8)),
+  new Qoute (1,"insperational","victor","petience pays but pains", new Date(2020,4,6)),
+  new Qoute (2,"love","tripplevic","there is no way to success but success is the way", new Date(2020 ,9, 10)),
+  new Qoute (3,"mortivational","manuu","do not boast about tommorrow for you do not know what a day may bring forth" ,new Date (2000,6,12)),
+  new Qoute (4,"love","victa","petience pays but bail" ,new Date(2001,4,7)),
+  new Qoute (5,"insperational","victa","petience pays but bail" ,new Date (1990,6,8)),
 
-  
+
 ]
 addNewQoute(qoute){
   let qouteLength = this.qoutes.length;
-  qoute.type = qouteLength+1;
-  qoute.name = qouteLength+1;
-  qoute.description = qouteLength+1;
-  qoute.completeDate = new Date(qoute.completeDate)
+  qoute.id = qouteLength+1;
+ qoute.completeDate = new Date(qoute.completeDate)
   this.qoutes.push(qoute)
 }
 
